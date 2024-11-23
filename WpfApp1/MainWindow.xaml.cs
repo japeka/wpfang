4573ddf6061051590d39b744605c6375b8e4a4cc
Janne Kalliokulju <janne.kalliokulju@gmail.com>
2024-11-23 12:36:20 +0200

    apps copied

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Web.WebView2.Core;
+
+namespace WpfApp1
+{
+   /// <summary>
+   /// Interaction logic for MainWindow.xaml
+   /// </summary>
+   public partial class MainWindow : Window
+   {
+      public MainWindow()
+      {
+         InitializeComponent();
+      }
+
+      private void ButtonGo_Click(object sender, RoutedEventArgs e)
+      {
+         if (webView != null && webView.CoreWebView2 != null)
+         {
+            webView.CoreWebView2.Navigate(addressBar.Text);
+         }
+      }
+   }
+}
